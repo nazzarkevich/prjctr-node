88418cc91b51e8b5068ef0664b7ff714a5af0441
nazzarkevich <nazarjtk@gmail.com>
2021-10-17 19:53:10 +0300

    refactored 404 handle, add post handle

--- prjctr-node-task-1.js
+++ prjctr-node-task-1.js
@@ -3,26 +3,59 @@ const url = require("url");
 
 const hostname = "127.0.0.1";
 const port = 3000;
+const ROUTES = {
+  main: "/",
+  hello: "/hello",
+  goodbye: "/goodbye",
+};
+
+const STATUS_CODES = {
+  success: 200,
+  notFound: 404,
+  notAllowed: 405,
+};
+
+const REQUEST_METHODS = {
+  get: "GET",
+  post: "POST",
+};
 
 function helloRoute(query) {
-  return query.name ? `Hello ${query.name}` : "Hello World";
+  return {
+    response: query.name ? `Hello ${query.name}` : "Hello World",
+    code: STATUS_CODES.success,
+  };
 }
 
 function goodbyeRoute(query) {
-  return query.name ? `Goodbye ${query.name}` : "Goodbye";
+  return {
+    response: query.name ? `Goodbye ${query.name}` : "Goodbye",
+    code: STATUS_CODES.success,
+  };
 }
 
 function notFoundRoute() {
-  return "404";
+  return {
+    response: "Not found",
+    code: STATUS_CODES.notFound,
+  };
+}
+
+function setStatusCode(res, statusCode) {
+  res.statusCode = statusCode;
+}
+
+function writeBody(res, bodyContent) {
+  res.write(bodyContent);
 }
 
 function resolveRoutes(pathname, query) {
   switch (pathname) {
-    case "/":
-    case "/hello":
+    case ROUTES.main:
+    case ROUTES.hello:
       return helloRoute(query);
 
-    case "/goodbye":
+    case ROUTES.goodbye:
       return goodbyeRoute(query);
 
     default:
@@ -35,13 +68,21 @@ const server = http.createServer((req, res) => {
   const query = url.parse(req.url, true).query;
   const method = req.method;
 
-  res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
 
-  if (method === "GET") {
-    res.write(resolveRoutes(pathname, query));
-    res.end();
+  if (method === REQUEST_METHODS.get) {
+    const resolvedDetails = resolveRoutes(pathname, query);
+
+    setStatusCode(res, resolvedDetails.code);
+    writeBody(res, resolvedDetails.response);
   }
+
+  if (method === REQUEST_METHODS.post) {
+    setStatusCode(res, STATUS_CODES.notAllowed);
+    writeBody(res, "Not allowed");
+  }
+
+  res.end();
 });
 
 server.listen(port, hostname, () => {
